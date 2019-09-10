5563a318d8530a5e4ec00e1264d401655e8c7606
Phil Boothman <philboothman90@gmail.com>
2019-09-10 21:17:51 +0100

    Added profile page view and URL

--- accounts/views.py
+++ accounts/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def register(request):
     return render(request, 'accounts/register.html')
+
+def profile(request):
+    return render(request, 'accounts/profile.html')
