706d2fd5dae9d457137e62c3cee0408351cfc976
Phil Boothman <philboothman90@gmail.com>
2019-09-11 21:35:55 +0100

    Added the new user view

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .forms import MythMakerForm
 
 def register(request):
     return render(request, 'registration/register.html')
@@ -9,5 +11,20 @@ def profile(request):
     username = request.user.username
     context = {'username' : username}
     return render(request, 'registration/profile.html', context)
+
+def new_user(request):
+    if request.method == 'POST':
+        form = MythMakerForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_date['username']
+            password = form.cleaned_data['password1']
+            email = form.cleaned_data['email']
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            return render(request, 'registration/profile.html')
+    else:
+        form = MythMakerForm()
+    return render(request, 'registration/register.html')
 
 
