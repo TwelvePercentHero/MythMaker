75619ec591f8099415bbd967083510c3508bfdc5
Phil Boothman <philboothman90@gmail.com>
2019-09-11 21:21:07 +0100

    Added login decorator to profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     return render(request, 'registration/register.html')
 
+@login_required
 def profile(request):
     username = request.user.username
     context = {'username' : username}
