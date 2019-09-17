f1cb70676ac95295f552f4f48786f80c313988aa
Phil Boothman <philboothman90@gmail.com>
2019-09-17 13:51:40 +0100

    Updated login URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .forms import ExtendedAuthForm
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
     path('profile/', views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html')),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', authentication_form=ExtendedAuthForm)),
     path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
