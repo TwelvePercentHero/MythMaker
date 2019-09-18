bfff531fa99dd146076e4a6d8550833a2e035d46
Phil Boothman <philboothman90@gmail.com>
2019-09-18 21:04:29 +0100

    Updated User model

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class MythMaker(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tagline = models.TextField(max_length=100, blank=True)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
