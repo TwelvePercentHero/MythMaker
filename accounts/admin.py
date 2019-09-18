2458b219993e5963b54fdda462094a8a7e48f37b
Phil Boothman <philboothman90@gmail.com>
2019-09-18 20:54:10 +0100

    Extended User model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import MythMaker
+
+class MythMakerInline(admin.StackedInline):
+    model = MythMaker
+    can_delete = False
+    verbose_name_plural = 'MythMakers'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (MythMakerInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class MythMaker(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
