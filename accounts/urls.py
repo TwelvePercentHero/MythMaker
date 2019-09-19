556d414fcdf89785efe3230b4ce155077a12ffda
Phil Boothman <philboothman90@gmail.com>
2019-09-19 21:05:04 +0100

    Updated password reset function

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,13 +11,13 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html',
                                                 authentication_form=ExtendedAuthForm)),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html',
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/reset.html',
                                                                  html_email_template_name='registration/reset_email.html',
                                                                  subject_template_name='registration/reset_subject.txt',
                                                                  success_url='sent'),
                                                                  name='reset'),
-    path('password_reset/sent/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_sent.html'),
+    path('password_reset/sent/', auth_views.PasswordResetDoneView.as_view(template_name='registration/reset_sent.html'),
                                                                         name='reset_sent'),
-    path('password_reset_confirm/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/reset_confirm.html'),
                                                                                 name='reset_confirm')
 ]
