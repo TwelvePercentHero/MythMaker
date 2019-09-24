17a784f6b5955b3bde23d4a9f7910f075f1aa171
Phil Boothman <philboothman90@gmail.com>
2019-09-24 18:32:45 +0100

    Created Subscription model

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Subscription
 
-# Register your models here.
+class SubscriptionAdminStacked(admin.StackedInline):
+    model = Subscription
+
+admin.site.register(Subscription)
