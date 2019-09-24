6ae183220f58aa987daf643f9a41d1bd02eb5423
Phil Boothman <philboothman90@gmail.com>
2019-09-24 20:20:42 +0100

    Updated subscribe view

--- subscription/views.py
+++ subscription/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import MakePaymentForm
+from django.forms.forms import NON_FIELD_ERRORS
 from django.conf import settings
-from django.utils import timezone
+from .models import Subscriber
+from .forms import SubscriberPaymentForm
+
 import stripe
 
 @login_required
@@ -12,10 +17,15 @@ def benefits(request):
 @login_required
 def subscribe(request):
     if request.method == 'POST':
-        subscription_form = MakePaymentForm(request.POST)
-
-        if subscription_form.is_valid():
-            subscription = subscription_form.save(commit=False)
-            subscription_date = timezone.now()
-            subscription.save
+        form = SubscriberPaymentForm(request.POST)
+        if form.is_valid():
+            fee = settings.SUBSCRIPTION_PRICE
+            try:
+                stripe_customer = sub.charge(request, email, fee)
+            except stripe.StripeError as e:
+                form._errors[NON_FIELD_ERRORS] = form.error_class([e.args[0]])
+                return render(request, template, {'form' : form, 'STRIPE_PUBLISHABLE_KEY' : settings.STRIPE_PUBLISHABLE_KEY})
+    else:
+        form = SubscriberPaymentForm()
+    return render(request, 'accounts/profile.html')  
 
