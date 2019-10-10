92ee6f1745f7d7b8c38f21d0941056df20a17882
Phil Boothman <philboothman90@gmail.com>
2019-10-10 21:26:26 +0100

    Added Stripe Elements and initial JS code

--- main/static/js/stripe.js
+++ main/static/js/stripe.js
@@ -1,2 +1,13 @@
 var stripe = Stripe('pk_test_WSMHaOkBZMUYsHyaNDDEsLGD00wJHHzet3');
 var elements = stripe.elements();
+
+var style = {
+    base: {
+        fontSize: '16px',
+        color: '#32325d',
+    }
+};
+
+var card = elements.create('card', {style: style});
+
+card.mount('#card-element');
