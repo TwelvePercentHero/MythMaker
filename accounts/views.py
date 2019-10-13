94d624f877aec1bcf19e3ffedee0db4b1d08d0d1
Phil Boothman <philboothman90@gmail.com>
2019-10-13 15:43:29 +0100

    Worked on HTML email template

--- accounts/views.py
+++ accounts/views.py
@@ -8,8 +8,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.core import mail
+from django.utils.html import strip_tags
 from django.template.loader import render_to_string
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMultiAlternatives
 from django.views.generic import ListView
 from .forms import MythMakerForm, SubscriberForm
 from .tokens import account_activation_token
@@ -49,15 +51,15 @@ def register(request):
             user.save()
             current_site = get_current_site(request)
             mail_subject = 'Activate your MythMaker account'
-            message = render_to_string('registration/activation_email.html', {
+            html_message = render_to_string('registration/activation_email.html', {
                 'user' : user,
                 'domain' : current_site.domain,
                 'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
                 'token' : account_activation_token.make_token(user),
             })
+            plain_message: strip_tags(html_message)
             to_email = form.cleaned_data.get('email')
-            email = EmailMessage(mail_subject, message, to=[to_email])
-            email.send()
+            mail.send_mail(mail_subject, plain_message, to_email, html_message=html_message)
             return render(request, 'registration/activate.html')
     else:
         form = MythMakerForm()
