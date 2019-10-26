7d34da986fb74ea2ebae5950f0f78399d72c2c41
Phil Boothman <philboothman90@gmail.com>
2019-10-26 13:58:45 +0100

    Updated video and account models

--- accounts/models.py
+++ accounts/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django import forms
 from django.contrib.auth.models import User
 from django.conf import settings
+
+from video.models import Video
 
 import stripe
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 class MythMaker(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    tagline = models.TextField(max_length=100, blank=True)
-    bio = models.TextField(max_length=500, blank=True)
-    profile_image = models.ImageField(upload_to='profile_images', blank=True)
-    profile_header = models.ImageField(upload_to='profile_headers', blank=True)
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    tagline = models.TextField(max_length = 100, blank = True)
+    bio = models.TextField(max_length = 500, blank = True)
+    profile_image = models.ImageField(upload_to = 'profile_images', blank = True)
+    profile_header = models.ImageField(upload_to = 'profile_headers', blank = True)
+    uploaded_videos = models.ForeignKey(Video, on_delete = models.CASCADE, blank = True, null = True)
 
     class Meta:
         verbose_name = 'MythMaker'
@@ -35,9 +37,9 @@ class Membership(models.Model):
         (FREE, 'Free'),
         (PREMIUM, 'Premium'),
     ]
-    membership_type = models.CharField(choices=MEMBERSHIP_CHOICES, blank=False, max_length=2)
-    price = models.IntegerField(default=0)
-    stripe_plan_id = models.CharField(max_length=40)
+    membership_type = models.CharField(choices = MEMBERSHIP_CHOICES, blank = False, max_length = 2)
+    price = models.IntegerField(default = 0)
+    stripe_plan_id = models.CharField(max_length = 40)
 
     class Meta:
         verbose_name = 'Membership'
@@ -49,9 +51,9 @@ class Membership(models.Model):
 DEFAULT_MEMBERSHIP_ID = 1
 
 class MythMakerMembership(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    stripe_customer_id = models.CharField(max_length=40)
-    membership = models.ForeignKey(Membership, on_delete=models.SET_NULL, null=True, default=DEFAULT_MEMBERSHIP_ID)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    stripe_customer_id = models.CharField(max_length = 40)
+    membership = models.ForeignKey(Membership, on_delete = models.SET_NULL, null = True, default = DEFAULT_MEMBERSHIP_ID)
 
     class Meta:
         verbose_name = 'MythMaker Membership'
@@ -62,21 +64,21 @@ class MythMakerMembership(models.Model):
 
 def post_save_mythmaker_membership_create(sender, instance, created, *args, **kwargs):
     if created:
-        MythMakerMembership.objects.get_or_create(user=instance)
+        MythMakerMembership.objects.get_or_create(user = instance)
 
-    mythmaker_membership, created = MythMakerMembership.objects.get_or_create(user=instance)
+    mythmaker_membership, created = MythMakerMembership.objects.get_or_create(user = instance)
 
     if mythmaker_membership.stripe_customer_id is None or mythmaker_membership.stripe_customer_id == '':
-        new_customer_id = stripe.Customer.create(email=instance.email)
+        new_customer_id = stripe.Customer.create(email = instance.email)
         mythmaker_membership.stripe_customer_id = new_customer_id['id']
         mythmaker_membership.save()
 
-post_save.connect(post_save_mythmaker_membership_create, sender=settings.AUTH_USER_MODEL)
+post_save.connect(post_save_mythmaker_membership_create, sender = settings.AUTH_USER_MODEL)
 
 class Subscription(models.Model):
-    mythmaker_membership = models.ForeignKey(MythMakerMembership, on_delete=models.CASCADE)
-    stripe_subscription_id = models.CharField(max_length=40)
-    active = models.BooleanField(default=True)
+    mythmaker_membership = models.ForeignKey(MythMakerMembership, on_delete = models.CASCADE)
+    stripe_subscription_id = models.CharField(max_length = 40)
+    active = models.BooleanField(default = True)
 
     class Meta:
         verbose_name = 'Subscription'
--- video/models.py
+++ video/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length = 100)
+    uploaded_by = models.OneToOneField(User, on_delete = models.CASCADE, blank = True, null = True)
+    date_posted = models.DateTimeField(auto_now_add = True)
+    video_file = models.FileField(upload_to = 'videos')
+    thumbnail = models.FileField(upload_to = 'thumbnails')
+
+    def __str__(self):
+        return self.title
