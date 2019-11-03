2fc05131c92f9105608877e209a28db1adab3820
Phil Boothman <philboothman90@gmail.com>
2019-11-03 11:52:56 +0000

    Added test models for video app

--- video/test_models.py
+++ video/test_models.py
@@ -1,1 +1,8 @@
 from django.test import TestCase
+from .models import Video
+
+class TestVideoModel(TestCase):
+
+    def test_title_is_a_string(self):
+        video = Video(title = 'Test Video Title')
+        self.assertEqual(video.title, str(video))
