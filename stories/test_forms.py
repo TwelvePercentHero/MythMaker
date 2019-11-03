481ea053f141d92c369ccf4b806d5332404654b8
Phil Boothman <philboothman90@gmail.com>
2019-11-03 10:30:02 +0000

    Wrote form tests for story app

---
+++ stories/test_forms.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from .forms import StoryUpload
+
+testfile = (
+    b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x00\x00\x00\x21\xf9\x04'
+    b'\x01\x0a\x00\x01\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02'
+    b'\x02\x4c\x01\x00\x3b'
+    )
+
+class TestStoryForm(TestCase):
+
+    def test_story_upload_with_images(self):
+        form = StoryUpload({'title' : 'Test Title',
+                            'synopsis' : 'Test Synopsis',
+                            'story' : 'This is a test story',
+                            'cover_image' : SimpleUploadedFile('small.gif', testfile, content_type = 'image/gif'),
+                            'story_thumbnail' : SimpleUploadedFile('small.gif', testfile, content_type = 'image/gif')})
+        self.assertTrue(form.is_valid)
+    
+    def test_story_upload_without_images(self):
+        form = StoryUpload({'title' : 'Test Title',
+                            'synopsis' : 'Test Synopsis',
+                            'story' : 'This is a test story'})
+        self.assertTrue(form.is_valid)
--- stories/test_views.py
+++ stories/test_views.py
@@ -1,10 +1,26 @@
-from django.test import TestCase, Client
+'''from django.test import TestCase, Client
 from .models import Story
 
+from accounts.forms import MythMakerForm, ExtendedAuthForm
+
 class TestStoryViews(TestCase):
+
+    c = Client()
+
+    def setUp(self):
+        user = MythMakerForm({'username': 'Test',
+                                    'email': 'mythmakerinchief@gmail.com',
+                                    'password1': 'testing123',
+                                    'password2': 'testing123'})
+        test_user = user.save()
+        authenticate_test = ExtendedAuthForm({'username': 'Test',
+                                                'password': 'testing123'})
+        authenticate_test.save()
+
+        logged_in = self.c.login(username = 'Test', password = 'testing123')
     
     def test_storylist(self):
-        page = self.client.get('/storylist/')
+        page = self.c.get('/storylist/')
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, 'stories/storylist.html')
 
@@ -12,5 +28,5 @@ class TestStoryViews(TestCase):
         story = Story(title = 'Test Story', synopsis = 'Test synopsis')
         story.cover_image = open('media/profile_headers/profile-header.png')
         story.save()
-        page = self.client.get('/story/{}'.format(story.id))
-        self.assertEqual(page.status_code, 200)
+        page = self.c.get('/story/{}'.format(story.id))
+        self.assertEqual(page.status_code, 200)'''
