8b14e6d21bf887c3449d0bfe018ed9ff123b5260
Phil Boothman <philboothman90@gmail.com>
2019-11-03 15:12:01 +0000

    Added js to toggle visibility of results divs

---
+++ main/static/js/main.js
@@ -0,0 +1,15 @@
+$("#button").click(function() {
+    $("#video-results").toggle();
+});
+
+$("#button2").click(function() {
+    $("#story-results").toggle();
+});
+
+$("#button3").click(function() {
+    $("#audio-results").toggle();
+});
+
+$("#button4").click(function() {
+    $("#user-results").toggle();
+});
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.db.models import Q
 from django.contrib.auth.models import User
 from itertools import chain
+from accounts.models import MythMakerMembership
 from video.models import Video
 from stories.models import Story
 from audio.models import Audio
@@ -14,9 +15,15 @@ def index(request):
     return render(request, 'main/index.html', context)
 
 def searchresults(request):
-    videos = Video.objects.filter(Q(title__icontains = request.GET['q']) | Q(description__icontains = request.GET['q']))
-    stories = Story.objects.filter(Q(title__icontains = request.GET['q']) | Q(synopsis__icontains = request.GET['q']))
-    audios = Audio.objects.filter(Q(title__icontains = request.GET['q']) | Q(description__icontains = request.GET['q']))
-    mythmakers = User.objects.filter(Q(username__icontains = request.GET['q']))
-    context = {'videos' : videos, 'stories' : stories, 'audios' : audios, 'mythmakers' : mythmakers}
+    user = request.user
+    if request.user.is_authenticated:
+        mythmaker_membership = MythMakerMembership.objects.get(user = user)
+    else:
+        mythmaker_membership = None
+    query = request.GET['q']
+    videos = Video.objects.filter(Q(title__icontains = query) | Q(description__icontains = query))
+    stories = Story.objects.filter(Q(title__icontains = query) | Q(synopsis__icontains = query))
+    audios = Audio.objects.filter(Q(title__icontains = query) | Q(description__icontains = query))
+    mythmakers = User.objects.filter(Q(username__icontains = query))
+    context = {'user' : user, 'mythmaker_membership' : mythmaker_membership, 'query' : query, 'videos' : videos, 'stories' : stories, 'audios' : audios, 'mythmakers' : mythmakers}
     return render(request, 'main/searchresults.html', context)
