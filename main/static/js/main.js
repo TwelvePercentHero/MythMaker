fb77d66232e3f02f4681bfdfad4805b386dddd6c
Phil Boothman <philboothman90@gmail.com>
2019-11-03 16:27:13 +0000

    Added js to show/hide search result sections

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -1,15 +1,27 @@
-$("#button").click(function() {
-    $("#video-results").toggle();
-});
-
-$("#button2").click(function() {
-    $("#story-results").toggle();
-});
-
-$("#button3").click(function() {
-    $("#audio-results").toggle();
-});
-
-$("#button4").click(function() {
-    $("#user-results").toggle();
+$(document).ready(function() {
+    $("#show-videos").click(function() {
+        $("#video-results").removeClass("d-none").addClass("d-block");
+        $("#story-results").removeClass("d-block").addClass("d-none");
+        $("#audio-results").removeClass("d-block").addClass("d-none");
+        $("#user-results").removeClass("d-block").addClass("d-none");
+    });
+    $("#show-stories").click(function() {
+        $("#video-results").removeClass("d-block").addClass("d-none");
+        $("#story-results").removeClass("d-none").addClass("d-block");
+        $("#audio-results").removeClass("d-block").addClass("d-none");
+        $("#user-results").removeClass("d-block").addClass("d-none");
+    });
+    $("#show-audios").click(function() {
+        $("#video-results").removeClass("d-block").addClass("d-none");
+        $("#story-results").removeClass("d-block").addClass("d-none");
+        $("#audio-results").removeClass("d-none").addClass("d-block");
+        $("#user-results").removeClass("d-block").addClass("d-none");
+    });
+    $("#show-stories").click(function() {
+        $("#video-results").removeClass("d-block").addClass("d-none");
+        $("#story-results").removeClass("d-block").addClass("d-none");
+        $("#audio-results").removeClass("d-block").addClass("d-none");
+        $("#user-results").removeClass("d-none").addClass("d-block");
+    });
+    
 });
