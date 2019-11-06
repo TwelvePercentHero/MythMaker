f912656470c3cd7424b197706610bc6c992e5a3a
Phil Boothman <philboothman90@gmail.com>
2019-11-06 18:31:36 +0000

    Added like function to audio template

--- community/urls.py
+++ community/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('like_story/<int:story_id>', views.like_story, name = 'like_story'),
     path('like_video/<int:video_id>', views.like_video, name = 'like_video'),
+    path('like_audio/<int:audio_id>', views.like_audio, name = 'like_audio'),
 ]
