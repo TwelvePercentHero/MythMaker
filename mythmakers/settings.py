7aa3a9a6114e18e41edd0e3d20dd74f698450507
Phil Boothman <philboothman90@gmail.com>
2019-11-07 20:38:20 +0000

    Updated settings to test Travis error

--- mythmakers/settings.py
+++ mythmakers/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'mythmakers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mythmakersDB',
@@ -89,7 +89,18 @@ DATABASES = {
         'HOST' : 'localhost',
         'PORT' : '5432',
     }
-}
+}'''
+
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
