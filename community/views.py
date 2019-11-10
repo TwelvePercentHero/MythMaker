fe70cb81d43d4f37d7455c7b3fa12d5f1dbecf12
Phil Boothman <philboothman90@gmail.com>
2019-11-10 16:09:32 +0000

    Added messages to like views

--- community/views.py
+++ community/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
@@ -20,8 +21,10 @@ def like_story(request, story_id):
             new_like.save()
             story.story_likes += 1
             story.save()
+            messages.success(request, 'Story liked!')
             return redirect(reverse('story', kwargs = {'story_id' : story_id}))
         else:
+            messages.warning(request, 'You have already liked this story!')
             return redirect(reverse('story', kwargs = {'story_id': story_id}))
 
 @login_required
@@ -35,8 +38,10 @@ def like_video(request, video_id):
             new_like.save()
             video.video_likes += 1
             video.save()
+            messages.success(request, 'Video liked!')
             return redirect(reverse('video', kwargs = {'video_id' : video_id}))
         else:
+            messages.warning(request, 'You have already liked this video!')
             return redirect(reverse('video', kwargs = {'video_id' : video_id}))
 
 @login_required
@@ -50,7 +55,9 @@ def like_audio(request, audio_id):
             new_like.save()
             audio.audio_likes += 1
             audio.save()
+            messages.success(request, 'Audio liked!')
             return redirect(reverse('audio', kwargs = {'audio_id' : audio_id}))
         else:
+            messages.warning(request, 'You have already liked this audio!')
             return redirect(reverse('audio', kwargs = {'audio_id' : audio_id}))
 
