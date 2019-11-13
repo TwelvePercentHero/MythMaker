4763f85ac1b7103ff2274ba003a1c903cf74aa45
Phil Boothman <philboothman90@gmail.com>
2019-11-13 21:51:27 +0000

    Updated searchresults

--- main/views.py
+++ main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.db.models import Q
 from django.contrib.auth.models import User
-from itertools import chain
 from accounts.models import MythMakerMembership
 from video.models import Video
 from stories.models import Story
@@ -11,7 +10,11 @@ def index(request):
     latest_videos = Video.objects.order_by('-date_posted')[0:5]
     latest_stories = Story.objects.order_by('-date_posted')[0:5]
     latest_audio = Audio.objects.order_by('-date_posted')[0:5]
-    context = {'latest_videos' : latest_videos, 'latest_stories' : latest_stories, 'latest_audio' : latest_audio}
+    context = {
+        'latest_videos' : latest_videos,
+        'latest_stories' : latest_stories,
+        'latest_audio' : latest_audio
+        }
     return render(request, 'main/index.html', context)
 
 def about(request):
@@ -28,5 +31,13 @@ def searchresults(request):
     stories = Story.objects.filter(Q(title__icontains = query) | Q(synopsis__icontains = query))[0:5]
     audios = Audio.objects.filter(Q(title__icontains = query) | Q(description__icontains = query))[0:5]
     mythmakers = User.objects.filter(Q(username__icontains = query))[0:5]
-    context = {'user' : user, 'mythmaker_membership' : mythmaker_membership, 'query' : query, 'videos' : videos, 'stories' : stories, 'audios' : audios, 'mythmakers' : mythmakers}
+    context = {
+        'user' : user,
+        'mythmaker_membership' : mythmaker_membership,
+        'query' : query,
+        'videos' : videos,
+        'stories' : stories,
+        'audios' : audios,
+        'mythmakers' : mythmakers
+        }
     return render(request, 'main/searchresults.html', context)
