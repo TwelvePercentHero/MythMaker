ff670c788bb65ffd81a43e2f4124077413128976
Phil Boothman <philboothman90@gmail.com>
2019-11-15 18:25:26 +0000

    Updated styles and jQuery for searchresults

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -4,24 +4,40 @@ $(document).ready(function() {
         $("#story-results").removeClass("d-block").addClass("d-none");
         $("#audio-results").removeClass("d-block").addClass("d-none");
         $("#user-results").removeClass("d-block").addClass("d-none");
+        $("#show-videos").removeClass("deselected").addClass("selected");
+        $("#show-stories").removeClass("selected").addClass("deselected");
+        $("#show-audios").removeClass("selected").addClass("deselected");
+        $("#show-users").removeClass("selected").addClass("deselected");
     });
     $("#show-stories").click(function() {
         $("#video-results").removeClass("d-block").addClass("d-none");
         $("#story-results").removeClass("d-none").addClass("d-block");
         $("#audio-results").removeClass("d-block").addClass("d-none");
         $("#user-results").removeClass("d-block").addClass("d-none");
+        $("#show-videos").removeClass("selected").addClass("deselected");
+        $("#show-stories").removeClass("deselected").addClass("selected");
+        $("#show-audios").removeClass("selected").addClass("deselected");
+        $("#show-users").removeClass("selected").addClass("deselected");
     });
     $("#show-audios").click(function() {
         $("#video-results").removeClass("d-block").addClass("d-none");
         $("#story-results").removeClass("d-block").addClass("d-none");
         $("#audio-results").removeClass("d-none").addClass("d-block");
         $("#user-results").removeClass("d-block").addClass("d-none");
+        $("#show-videos").removeClass("selected").addClass("deselected");
+        $("#show-stories").removeClass("selected").addClass("deselected");
+        $("#show-audios").removeClass("deselected").addClass("selected");
+        $("#show-users").removeClass("selected").addClass("deselected");
     });
     $("#show-users").click(function() {
         $("#video-results").removeClass("d-block").addClass("d-none");
         $("#story-results").removeClass("d-block").addClass("d-none");
         $("#audio-results").removeClass("d-block").addClass("d-none");
         $("#user-results").removeClass("d-none").addClass("d-block");
+        $("#show-videos").removeClass("selected").addClass("deselected");
+        $("#show-stories").removeClass("selected").addClass("deselected");
+        $("#show-audios").removeClass("selected").addClass("deselected");
+        $("#show-users").removeClass("deselected").addClass("selected");
     });
     
 });
