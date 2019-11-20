4b075ea69f490b46e3700b45c1d41a30d8914a85
Phil Boothman <philboothman90@gmail.com>
2019-11-20 10:21:53 +0000

    Changed test views for audio

--- audio/test_views.py
+++ audio/test_views.py
@@ -6,19 +6,27 @@ from audio.views import audio, audiolist
 
 class TestAudioListView(TestCase):
 
-    def setUp(self):
-        self.factory = RequestFactory()
-        self.user = User.objects.create_user(
+    def test_audiolist(self):
+        user = User.objects.create_user(
             username = 'TestUser',
             email = 'test@test.com',
             password = 'testpassword'
         )
-        test_membership = Membership.objects.create(
+        membership = Membership.objects.create(
             membership_type = 'FR'
         )
-        test_mythmaker_membership = MythMakerMembership.objects.create(
-            user = self.user,
-            membership = test_membership
+        page = self.client.get('/audiolist/')
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, 'audio/audiolist.html')
+
+    def test_audio(self):
+        user = User.objects.create_user(
+            username = 'TestUser',
+            email = 'test@test.com',
+            password = 'testpassword'
+        )
+        membership = Membership.objects.create(
+            membership_type = 'FR'
         )
         audio = Audio.objects.create(
             title = 'Test Audio',
@@ -26,17 +34,8 @@ class TestAudioListView(TestCase):
             audio_file = 'media/audio/test_audio_file.mp3',
             cover_image = 'media/cover_images/test-header.jpg',
             audio_thumbnail = 'media/thumbnails/test-thumb-1.jpg',
-            creator = self.user
+            creator = user
         )
-
-    def test_audiolist(self):
-        request = self.factory.get('/audiolist/')
-        request.user = self.user
-        response = audiolist(request)
-        self.assertEqual(response.status_code, 200)
-
-    def test_audio(self):
-        audio = Audio.objects.get(title = 'Test Audio')
-        response = self.client.get('/audio/{}'.format(audio.id))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'audio/audio.html')
+        page = self.client.get('/audio/{}'.format(audio.id))
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, 'audio/audio.html')
